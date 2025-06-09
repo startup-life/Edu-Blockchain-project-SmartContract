9cf97599d17835c626c55ef92a3252eba38d4fc1
RyeinKim <ryein6896@gmail.com>
2025-06-09 14:27:38 +0900

    update code

--- contracts/CommuynityToken.sol
+++ contracts/CommuynityToken.sol
@@ -7,9 +7,4 @@ contract CommunityToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("CommunityToken", "CTK") {
         _mint(msg.sender, initialSupply * (10 ** decimals()));
     }
-
-    // 서버 지갑(소유자)만 호출 가능: 토큰 추가 발행
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount * (10 ** decimals()));
-    }
 }
